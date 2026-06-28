f31c4e7bebbccc273cd42076e6ee26cc0e14c2e6
Pixeleyesd <juliushwashere@gmail.com>
2026-06-28 17:41:56 +1200

    fix player moonwalking and jumping animation

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,45 +3,32 @@ extends CharacterBody2D
 
 @onready var animated_sprite = $AnimatedSprite2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 400.0
+const JUMP_VELOCITY = -475.0
 
-const PIVOT_X = -13.0
-
-func _process(delta):	
+func _process(delta):
 	if Input.is_action_pressed("ui_right"):
 		animated_sprite.play("run")
+		animated_sprite.flip_h = false
 	elif Input.is_action_pressed("ui_left"):
 		animated_sprite.play("run")
+		animated_sprite.flip_h = true
 	else:
 		animated_sprite.play("idle")
 	
-	position += velocity * delta
-	
-	# Determine which side of pivot the character is on
-	if position.x < PIVOT_X:
-		# Left side: flip and mirror position
-		animated_sprite.flip_h = true
-		position.x = 2 * PIVOT_X - position.x
-	else:
-		# Right side: normal
-		animated_sprite.flip_h = false
-	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+		animated_sprite.play("jump")
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		
-	if Input.is_action_just_pressed("ui_accept"):
-		animated_sprite.play("jump")
+	
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
 	move_and_slide()
